e30e0ea6441f9265fc1dde667de246d347484fc9
Semra Genç <semrademir292@gmail.com>
2025-06-30 23:38:39 +0300

    This is finish

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,15 +1,31 @@
 import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 function Users() {
     const [users, setUsers] = useState([]);
+    const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((res) => res.json()) //json olarak pars etmliyiz. Unutma
-            .then((data) => setUsers(data)) //Çıktısını al
-            //yani bu 2 Then i veri isterken kullanmalıyız.
-            .finally( () => setLoading(false));
+        // axios("https://jsonplaceholder.typicode.com/users")
+        //     .then((res) => {
+        //         setUsers(res.data);
+
+        //         axios(`https://jsonplaceholder.typicode.com/posts?userid=${res.data[0].id}`
+        //         ).then((res) => setPosts(res.data));
+        //     //json olarak pars etmliyiz. Unutma
+        //     //.then((data) => setUsers(data)) //Çıktısını al
+        //     //yani bu 2 Then i veri isterken kullanmalıyız.
+        //     })
+
+        //     .finally( () => setLoading(false));
+        getData();
     }, []);
+
+
+    const getData = async () => {
+        const users = await axios("https://jsonplaceholder.typicode.com/users");
+        console.log(users)
+    };
   return (
     <div>
         <h2>Users</h2>
@@ -21,6 +37,13 @@ function Users() {
         <ul>
             {users.map((user) => (
                 <li key={user.id}>{user.name}</li>
+                ))}
+        </ul>
+
+        <h2>Posts</h2>
+        <ul>
+            {posts.map((post) => (
+                <li key={post.id}>{post.title}</li>
                 ))}
         </ul>
     </div>
